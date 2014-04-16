c98a6e1d9d98a83af57361cdd98f403f2193d135
Lummer <youp_hulsebos_@hotmail.com>
2014-04-16 21:32:20 +0200

    Backend solution added

---
+++ MobileDiscussie/Backend/Controllers/RequestController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Web.Http;
+
+namespace MobileDiscussie.Controllers
+{
+    public class RequestController : ApiController
+    {
+        // GET api/values/5
+        public string Get(int id)
+        {
+            Thread.Sleep(id);
+
+            return string.Format(CultureInfo.InvariantCulture, "Waited for {0} milliseconds", id);
+        }
+    }
+}
